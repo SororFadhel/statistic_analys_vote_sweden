7a30d9fec19517ef92947ad80ee745e6f05d6eb9
Zinah Alqeji <zinah.poulus@gmail.com>
2026-04-14 09:15:02 +0200

    polish intro file

--- js/intro.js
+++ js/intro.js
@@ -58,7 +58,7 @@ if (!dbInfoOk) {
   ## 📊 Data Preview
   `);
 
-  // ✅ ALSO add safety check
+  //add safety check
   if (Array.isArray(countyInfo)) {
     tableFromData({
       data: countyInfo.slice(0, 5)
