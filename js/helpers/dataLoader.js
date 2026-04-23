1990472862262d9f5f7d7967086527b4fa8f9f54
Sumeya <sumeyacabuk@hotmail.com>
2026-04-23 12:03:37 +0200

    Fix income analysis: resolve db config, fix dropdown gender filter, update unemployment map field names, redesign page with hero banner, styled cards, dark table header, replace unemployment scatter with lan bar chart sorted by unemployment level

--- js/helpers/dataLoader.js
+++ js/helpers/dataLoader.js
@@ -3,8 +3,8 @@
 
 // SQLITE
 dbQuery.use('counties-sqlite');
-// export let countyInfo = await dbQuery('SELECT * FROM countyInfo');
-export let unemployment = await dbQuery('SELECT * FROM arbetsloshet_clean');
+export let countyInfo = await dbQuery('SELECT * FROM countyInfo');
+export let unemployment = await dbQuery('SELECT * FROM arbetsloshet_by_lan');
 export let income = await dbQuery('SELECT * FROM income_kommun');
 export let lanKommun = await dbQuery('SELECT * FROM lan_kommun');
 
